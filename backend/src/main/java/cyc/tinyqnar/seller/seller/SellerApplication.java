67f40da439ff593e5738f8faced9f2d4e1f1565d
zilchfp <zilchfp@gmail.com>
2018-06-02 15:01:47 +0800

    add db

---
+++ backend/src/main/java/cyc/tinyqnar/seller/seller/SellerApplication.java
@@ -0,0 +1,12 @@
+package cyc.tinyqnar.seller.seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellerApplication.class, args);
+    }
+}
